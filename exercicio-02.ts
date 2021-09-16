492ee3029d2c805c7a747b3d3527a9bb963a8d7e
Raquel Cabral <raquel.cabral.28@gmail.com>
2021-09-16 15:40:01 -0300

    Update exercicio-02.ts

--- exercicio-02.ts
+++ exercicio-02.ts
@@ -26,6 +26,7 @@ console.log(ola());
 
 const array= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
+//ARROW FUNCTION
 const elementosPares = array.filter(x => x % 2 ==0 );
 
 console.log(elementosPares);
